3a90c53fc4324ec05c7045a9a6b0e47b25f8c8c6
rajuvaghela <raju.vaghela@atharvasystem.com>
2022-04-18 16:54:07 +0530

    #lifecycle -Understand react native lifecycle method and print log for that.

--- App.js
+++ App.js
@@ -2,28 +2,19 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './components/Homescreen'
-import AboutScreen from './components/Aboutscreen'
-import ProfileDetailscreen from './components/ProfileDetailscreen'
-import LoginScreen from './components/LoginScreen'
-import ChangePasswordScreen from './components/ChangePasswordScreen';
-import ProfileScreen from './components/ProfileScreen'
-
-
+import LifeCycleScreen from './src/LifeCycleScreen'
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Navigator initialRouteName="LifeCycle">
+        {/* <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="ProfileDetail" component={ProfileDetailscreen} />
-        {/* <Stack.Screen name="About" component={AboutScreen} options={{ title: 'Overview' }} /> */}
-        {/* <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={Detailscreen} /> */}
+        <Stack.Screen name="ProfileDetail" component={ProfileDetailscreen} /> */}
+     <Stack.Screen name='LifeCycle' component={LifeCycleScreen} />
 
       </Stack.Navigator>
     </NavigationContainer>
