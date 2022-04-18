14d17552c8b707f46afef33dbb874857718432ca
rajuvaghela <raju.vaghela@atharvasystem.com>
2022-04-18 16:56:50 +0530

    lifecycle -understand lifecycle method and print logs for that.

--- src/LifeCycleScreen.js
+++ src/LifeCycleScreen.js
@@ -1,74 +1,80 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 export default class LifeCycleScreen extends React.Component {
   constructor(props) {
     super(props);
-    console.log("constructor called.");
+    console.log('constructor called.');
     this.state = {
       number: 0,
       mountButtonTitle: 'UNMOUNT',
-    }
+    };
     this.showCounter = true;
-  };
+  }
 
   incrementNumber() {
-    this.setState({number: this.state.number + 1})
+    this.setState({number: this.state.number + 1});
   }
 
   toggleCounter() {
-    if(this.showCounter === true){
+    if (this.showCounter === true) {
       this.setState({mountButtonTitle: 'MOUNT'});
       this.showCounter = false;
       this.setState({number: 0});
-    }
-    else {
+    } else {
       this.setState({mountButtonTitle: 'UNMOUNT'});
       this.showCounter = true;
     }
   }
 
   componentWillMount() {
-    console.log("componentWillMount called.");
+    console.log('componentWillMount called.');
   }
 
   componentDidMount() {
-    console.log("componentDidMount called.");
+    console.log('componentDidMount called.');
   }
 
   componentWillReceiveProps(nextProp) {
-    console.log("componentWillReceiveProps called.");
+    console.log('componentWillReceiveProps called.');
   }
 
   shouldComponentUpdate(nextProp, nextState) {
-    console.log("shouldComponentUpdate called.");
+    console.log('shouldComponentUpdate called.');
     return true;
   }
 
   componentWillUpdate(nextProp, nextState) {
-    console.log("componentWillUpdate called.");
+    console.log('componentWillUpdate called.');
   }
 
   componentDidUpdate(prevProp, prevState) {
-    console.log("componentDidUpdate called.");
+    console.log('componentDidUpdate called.');
   }
 
   componentWillUnmount() {
-    console.log("componentWillUnmount called.");
+    console.log('componentWillUnmount called.');
   }
 
   componentDidCatch(error, info) {
-    console.log("componentDidCatch called.");
+    console.log('componentDidCatch called.');
     //Handle error.
   }
 
   render() {
-    console.log("render called");
+    console.log('render called');
     return (
       <View style={styles.container}>
-        {this.showCounter ? <Text style={styles.text}>{this.state.number}</Text> : null}
-        {this.showCounter ? <Button onPress={this.incrementNumber.bind(this)} title="INCREMENT"/> : null}
-        <Button onPress={this.toggleCounter.bind(this)} title={this.state.mountButtonTitle}/>
+        {this.showCounter ? (
+          <Text style={styles.text}>{this.state.number}</Text>
+        ) : null}
+        {this.showCounter ? (
+          <Button onPress={this.incrementNumber.bind(this)} title="INCREMENT" />
+        ) : null}
+        <Button
+          onPress={this.toggleCounter.bind(this)}
+          title={this.state.mountButtonTitle}
+        />
       </View>
     );
   }
@@ -84,5 +90,5 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 31,
     marginBottom: 10,
-  }
+  },
 });
